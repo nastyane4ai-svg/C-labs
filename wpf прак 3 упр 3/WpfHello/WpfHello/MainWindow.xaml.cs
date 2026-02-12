a9774bb0b21b9517ff5cd565b1eea6ceb89e0c60
nastyane4ai-svg <nastya.ne4ai@gmail.com>
2026-02-12 18:38:32 +0300

    Praktika 3

---
+++ wpf прак 3 упр 3/WpfHello/WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isDataDirty = false;
+        string nameFile = "username.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbl.Content = "Добрый день!";
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+            Top = 25;
+            Left = 25;
+
+        }
+
+        private void SetBut()
+        {
+            System.IO.StreamWriter sw = null;
+            try
+            {
+                sw = new System.IO.StreamWriter(nameFile);
+                sw.WriteLine(setText.Text);
+
+                retBut.IsEnabled = true;
+                isDataDirty = false;
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+            }
+        }
+
+        private void RetBut()
+        {
+            System.IO.StreamReader sr = null;
+            try
+            {
+                sr = new System.IO.StreamReader(nameFile);
+                retLabel.Content =
+                    "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            try
+            {
+                switch (feSource.Name)
+                {
+                    case "setBut":
+                        SetBut();
+                        break;
+
+                    case "retBut":
+                        RetBut();
+                        break;
+                }
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            setBut.IsEnabled = true;
+            isDataDirty = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения ? "; MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public MyWindow myWin { get; set; }
+
+        private void New_Win_Click(object sender, RoutedEventArgs e)
+        {
+            if (myWin == null)
+                myWin = new MyWindow();
+
+            myWin.Owner = this;
+
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+
+            myWin.Show();
+        }
+    }
+}
